5a8092381d63f9e530ee7367181d6d900eae5516
michiru6828 <since_l999@yahoo.co.jp>
2023-04-21 08:46:30 -0700

    fixed style of options

--- src/components/pages/quiz/ShowQuestion.jsx
+++ src/components/pages/quiz/ShowQuestion.jsx
@@ -20,26 +20,28 @@ function ShowQuestion({
                 <div className='imgBox'>
                     <img src={answerCountry.flags.png} alt="" />
                 </div>
-                <div className='radiogroup'>
-                    {/* アロー関数が{}じゃない理由はreturn無しで描いてるから */}
-                    {options.map((option) => (
-                        <div key={uuidv4()} className="wrapper ">
-                            <input
-                                className='state'
-                                type="radio"
-                                name="countryOption"
-                                value={option}
-                                id={option}
-                                //checkedは視覚的に選択されてることを見せるのに必要(true/false)
-                                checked={selectedOption === option}
-                                onChange={handleOptionChange}
-                                disabled={isSubmitted} />
-                            <label htmlFor={option} className="label">
-                                <div className="indicator"></div>
-                                <span className='text'>{option}</span>
-                            </label>
-                        </div>
-                    ))}
+                <div className='optionsAndButton'>
+                    <div className='radiogroup ' style={{ alignItems: "left" }}>
+                        {/* アロー関数が{}じゃない理由はreturn無しで描いてるから */}
+                        {options.map((option) => (
+                            <div key={uuidv4()} className="wrapper ">
+                                <input
+                                    className='state'
+                                    type="radio"
+                                    name="countryOption"
+                                    value={option}
+                                    id={option}
+                                    //checkedは視覚的に選択されてることを見せるのに必要(true/false)
+                                    checked={selectedOption === option}
+                                    onChange={handleOptionChange}
+                                    disabled={isSubmitted} />
+                                <label htmlFor={option} className="label">
+                                    <div className="indicator"></div>
+                                    <span className='text'>{option}</span>
+                                </label>
+                            </div>
+                        ))}
+                    </div>
 
                     {/* Answer Button */}
                     <Button
@@ -56,6 +58,7 @@ function ShowQuestion({
                         className={"nextButton"}
                         isButtonHide={isAllAnswered || !isSubmitted}
                     />
+
                 </div>
             </div>
         </div>
